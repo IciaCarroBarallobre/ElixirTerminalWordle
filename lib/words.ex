3540128e189208246ff8732065931dfe71e3ff9d
IciaC <iciacarrobarallobre@gmail.com>
2022-11-11 21:21:01 +0100

    Use GenServer as a MapSet of words and not a Module attribute to allow changes on it

--- lib/words.ex
+++ lib/words.ex
@@ -1,47 +1,67 @@
 defmodule WordleTerminalGame.Words do
+  use GenServer
+
   @moduledoc """
-  Words has a module attribute which is a list that contains tuples of three elements ``(answer, explanation, clue)``.
-  - The first element of the tuple, is the Wordle answer.
-  - The second element of the tuple, is a detail about the answer.
-  - The third element of the tuple, is a clue about the answer.
+  Words is a GenServer Callback Module which contains a MapSet.
+  Its functionality is return a random element and delete this element from the MapSet.
+
+  In our case element is a tuple composed of {word, explanation, clue}.
   """
-  @available_words [
-    {"elixir",
-     "Elixir is a dynamic, functional language for building scalable and maintainable applications.
-      It runs on the Erlang VM, known for creating low-latency, distributed, and fault-tolerant systems.",
-     "It's a language."},
-    {"erlang",
-     "Erlang is a programming language used to build massively scalable soft real-time systems with requirements
-      on high availability. It runs on the Erlang VM, same as Elixir.", "It's a language."},
-    {"strings", "Strings in Elixir are UTF-8 encoded binaries.", "Data type."},
-    {"floats",
-     " Floats (floating points) are not useful for representing money because cannot be stored exactly as it is in the memory.
-      You could use instead Integers.", "Data Type"},
-    {"enum",
-     "Enum module provides a set of algorithms to work with enumerables (such as lists, maps and ranges).",
-     "A Module"},
-    {"livebook",
-     "Livebook is an app to write interactive & collaborative code notebooks in Elixir.",
-     "You can create notebooks with it."},
-    {"mix",
-     "Mix is a build tool that ships with Elixir that provides tasks for creating, compiling,
-     testing your application, managing its dependencies and much more.", "Build tool."},
-    {"iex", "IEx is the Elixir interactive shell.", "A shell."},
-    {"exunit", "ExUnit is a testing framework for Elixir. ", "A framework for testing."},
-    {"exdoc",
-     "`mix docs` uses ExDoc to generate a static web page from the project documentation.",
-     "A framework for documentation."}
-    # TODO: Add more words
-  ]
+  def start_link(_opts) do
+    GenServer.start_link(__MODULE__, [])
+  end
 
   @doc """
-  Returns a list of tuples, its first element is a word related with Elixir,
-  its second element is a detail/comment about the word and,
-  its third element is a clue to guess the word.
+  Returns a
   """
+  def pop(pid) do
+    GenServer.call(pid, :pop)
+  end
 
-  @spec get :: [{binary, binary, binary}]
-  def get() do
-    @available_words
+  # Server callbacks
+  @impl true
+  def handle_call(:pop, _from, state) do
+    unless Enum.empty?(state) do
+      chosen_word = Enum.random(state)
+      new_state = MapSet.delete(state, chosen_word)
+      {:reply, chosen_word, new_state}
+    else
+      {:reply, nil, state}
+    end
+  end
+
+  @impl true
+  def init(_opts) do
+    available_words =
+      MapSet.new([
+        {"elixir",
+         "Elixir is a dynamic, functional language for building scalable and maintainable applications.
+        It runs on the Erlang VM, known for creating low-latency, distributed, and fault-tolerant systems.",
+         "It's a language."},
+        {"erlang",
+         "Erlang is a programming language used to build massively scalable soft real-time systems with requirements
+          on high availability. It runs on the Erlang VM, same as Elixir.", "It's a language."},
+        {"strings", "Strings in Elixir are UTF-8 encoded binaries.", "Data type."},
+        {"floats",
+         " Floats (floating points) are not useful for representing money because cannot be stored exactly as it is in the memory.
+          You could use instead Integers.", "Data Type"},
+        {"enum",
+         "Enum module provides a set of algorithms to work with enumerables (such as lists, maps and ranges).",
+         "A Module"},
+        {"livebook",
+         "Livebook is an app to write interactive & collaborative code notebooks in Elixir.",
+         "You can create notebooks with it."},
+        {"mix",
+         "Mix is a build tool that ships with Elixir that provides tasks for creating, compiling,
+         testing your application, managing its dependencies and much more.", "Build tool."},
+        {"iex", "IEx is the Elixir interactive shell.", "A shell."},
+        {"exunit", "ExUnit is a testing framework for Elixir. ", "A framework for testing."},
+        {"exdoc",
+         "`mix docs` uses ExDoc to generate a static web page from the project documentation.",
+         "A framework for documentation."}
+        # TODO: Add more words
+      ])
+
+    {:ok, available_words}
   end
 end
